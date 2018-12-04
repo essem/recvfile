b6a8f09e66013168df411aa08a6bb6eceeb26849
essem <soomin@gmail.com>
2018-12-04 15:51:50 +0900

    Support multiple connection mode
    Append data event connection is closed and  connected again

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	outFile := flag.String("o", "", "Output file name")
 	echo := flag.Bool("e", false, "Send back recevied bytes")
 	verbose := flag.Bool("v", false, "Verbose")
+	multiple := flag.Bool("m", false, "Multiple connection")
 	flag.Parse()
 
 	bindAddr := fmt.Sprintf(":%d", *port)
@@ -50,71 +51,89 @@ func main() {
 		return
 	}
 
-	defer conn.Close()
-
 	fmt.Printf("Connection from %s\n", conn.RemoteAddr())
 
 	totalReceived := 0
 
 	for {
-		buf := make([]byte, *bufSize)
-		received, err := conn.Read(buf)
-		if err == io.EOF {
-			fmt.Println("Connection closed")
-			if received != 0 {
-				fmt.Printf("Unexpected: %d", received)
-				return
+		received := recv(conn, *bufSize, *verbose, *echo, file)
+		if received == 0 {
+			if *multiple {
+				conn.Close()
+				conn, err = ln.Accept()
+				if err != nil {
+					fmt.Printf("Failed to accept: %v\n", err)
+					return
+				}
+			} else {
+				fmt.Println("Connection closed")
+				break
 			}
-			break
-		} else if err != nil {
-			fmt.Printf("Failed to recv: %v\n", err)
-			return
 		}
-
 		totalReceived += received
-
-		if *verbose {
-			fmt.Printf("Recevied: %d\n", received)
-		}
-
-		if *echo {
-			sent := 0
-			for sent < received {
-				n, err := conn.Write(buf[sent:received])
-				if err != nil {
-					fmt.Printf("Failed to send: %v\n", err)
-					return
-				}
-
-				if *verbose {
-					fmt.Printf("Sent: %d\n", n)
-				}
-
-				sent += n
-			}
-		}
-
-		if file != nil {
-			written := 0
-			for written < received {
-				n, err := file.Write(buf[written:received])
-				if err != nil {
-					fmt.Printf("Failed to write: %v\n", err)
-					return
-				}
-
-				if *verbose {
-					fmt.Printf("Written: %d\n", n)
-				}
-
-				written += n
-			}
-		}
 	}
 
 	if file != nil {
 		file.Close()
 	}
 
+	conn.Close()
+
 	fmt.Printf("Total recv: %d\n", totalReceived)
+}
+
+func recv(conn net.Conn, bufSize int, verbose bool, echo bool, file *os.File) int {
+
+	buf := make([]byte, bufSize)
+	received, err := conn.Read(buf)
+	if err == io.EOF {
+		if received != 0 {
+			fmt.Printf("Unexpected: %d", received)
+			os.Exit(1)
+		}
+		return 0
+	} else if err != nil {
+		fmt.Printf("Failed to recv: %v\n", err)
+		os.Exit(1)
+	}
+
+	if verbose {
+		fmt.Printf("Recevied: %d\n", received)
+	}
+
+	if echo {
+		sent := 0
+		for sent < received {
+			n, err := conn.Write(buf[sent:received])
+			if err != nil {
+				fmt.Printf("Failed to send: %v\n", err)
+				os.Exit(1)
+			}
+
+			if verbose {
+				fmt.Printf("Sent: %d\n", n)
+			}
+
+			sent += n
+		}
+	}
+
+	if file != nil {
+		written := 0
+		for written < received {
+			n, err := file.Write(buf[written:received])
+			if err != nil {
+				fmt.Printf("Failed to write: %v\n", err)
+				os.Exit(1)
+			}
+
+			if verbose {
+				fmt.Printf("Written: %d\n", n)
+			}
+
+			written += n
+		}
+	}
+
+	return received
 }
